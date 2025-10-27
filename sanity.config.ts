2649383353eab754256a438b09a565e4dec2993b
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-10-27 12:56:40 +0200

    added color gradient fields

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,6 +1,7 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
+import {colorInput} from '@sanity/color-input'
 import {schemaTypes} from './schemaTypes'
 
 export default defineConfig({
@@ -10,10 +11,7 @@ export default defineConfig({
   projectId: 'vh20xg14',
   dataset: 'production',
 
-  plugins: [
-    structureTool(),
-    visionTool(),
-  ],
+  plugins: [structureTool(), visionTool(), colorInput()],
 
   schema: {
     types: schemaTypes,
